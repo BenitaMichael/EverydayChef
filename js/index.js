2134e46f43c716b0b84c4eab0bd49fc6e769a3b9
Abasilim <kabasilim@github.com>
2024-02-03 17:09:38 -0600

    Login page

--- js/index.js
+++ js/index.js
@@ -1,73 +1,33 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+let title = document.getElementById("title");
+let nameField_First = document.getElementById("nameField_First");
+let nameField_Last = document.getElementById("nameField_Last");
+let nameField_Middle = document.getElementById("nameField_Middle");
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn= document.getElementById("signinBtn");
+let password2Field= document.getElementById("password2Field");
+let emailField= document.getElementById("emailField");
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
 
-	validateInputs();
-});
 
-const setError = (element, message) => {
-	const inputControl = element.parentElement;
-	const errorDisplay = inputControl.querySelector('.error');
-
-	errorDisplay.innerText = message;
-	inputControl.classList.add('error');
-	inputControl.classList.remove('success')
+signinBtn.onclick = function(){
+	nameField_First.style.maxHeight = "0";
+	nameField_Last.style.maxHeight = "0";
+	nameField_Middle.style.maxHeight = "0";
+	emailField.style.maxHeight = "0";
+	password2Field.style.maxHeight = "0";
+	title.innerHTML = "EverydayChef";
+	signupBtn.classList.add("disable");
+	signinBtn.classList.remove("disable");
 }
 
-const setSuccess = element => {
-	const inputControl = element.parentElement;
-	const errorDisplay = inputControl.querySelector('.error');
 
-	errorDisplay.innerText = '';
-	inputControl.classList.add('success');
-	inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+signupBtn.onclick = function(){
+	nameField_First.style.maxHeight = "60px";
+	nameField_Last.style.maxHeight = "60px";
+	nameField_Middle.style.maxHeight = "60px";
+	emailField.style.maxHeight = "60px";
+	password2Field.style.maxHeight = "60px";
+	title.innerHTML = "EverydayChef";
+	signupBtn.classList.remove("disable");
+	signinBtn.classList.add("disable");
 }
-
-const validateInputs = () => {
-	const usernameValue = username.value.trim();
-	const emailValue = email.value.trim();
-	const passwordValue = password.value.trim();
-	const password2Value = password2.value.trim();
-
-	if (usernameValue === '') {
-		setError(username, 'Username is required');
-	} else {
-		setSuccess(username);
-	}
-
-	if (emailValue === '') {
-		setError(email, 'Email is required');
-	} else if (!isValidEmail(emailValue)) {
-		setError(email, 'Provide a valid email address');
-	} else {
-		setSuccess(email);
-	}
-
-	if (passwordValue === '') {
-		setError(password, 'Password is required');
-	} else if (passwordValue.length < 8) {
-		setError(password, 'Password must be at least 8 character.')
-	} else {
-		setSuccess(password);
-	}
-
-	if (password2Value === '') {
-		setError(password2, 'Please confirm your password');
-	} else if (password2Value !== passwordValue) {
-		setError(password2, "Passwords doesn't match");
-	} else {
-		setSuccess(password2);
-	}
-
-};
-
