ee47b54d5d9bab4a6c0740668bef788e53f60205
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-02-10 14:23:55 +0000

    modification of js

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,17 +1,25 @@
-let signinBtn= document.getElementById("signinBtn");
 let title = document.getElementById("title");
-let Password = document.getElementById("Password");
+let nameField_First = document.getElementById("nameField_First");
+let nameField_Last = document.getElementById("nameField_Last");
+let nameField_Middle = document.getElementById("nameField_Middle");
+let signupBtn = document.getElementById("signupBtn");
+let Password = document.getElementById("Password")
+let password2Field= document.getElementById("password2Field");
 let emailField= document.getElementById("emailField");
 
 
-signinBtn.onclick = function(){
+signupBtn.onclick = function(){
+	nameField_First.style.maxHeight = "60px";
+	nameField_Last.style.maxHeight = "60px";
+	nameField_Middle.style.maxHeight = "60px";
 	emailField.style.maxHeight = "60px";
 	Password.style.maxHeight = "60px";
-	title.innerHTML = "EverydayChef";
+	password2Field.style.maxHeight = "60px";
+	title.innerHTML = "Create account";
 }
   
   addEventListener('click', function() {
-    alert('Successful!');
-    window.location.href = 'search.html';
+    alert('Sign Up Successful!');
+    window.location.href = 'sign_in.html';
   }
   );
