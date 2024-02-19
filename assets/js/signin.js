6958cf5d0e4f6e52ec8832a07e56f0ef087aeb12
Abasilim <kabasilim@github.com>
2024-02-19 00:36:56 -0600

    clean up and comments

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,11 +1,15 @@
+// Get references to various elements in the document
 let title = document.getElementById("title");
 let signinBtn = document.getElementById("signupBtn");
 let Password = document.getElementById("Password")
 let emailField= document.getElementById("emailField");
 
 
+// Function triggered when signup button is clicked
 signinBtn.onclick = function(){
+	// Adjust the maximum height of various input fields to reveal them
 	emailField.style.maxHeight = "60px";
 	Password.style.maxHeight = "60px";
+	// Update the title of the form
 	title.innerHTML = "EverydayChef";
 }
--- forms/forgot_password.php
+++ forms/forgot_password.php
@@ -8,12 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "Password Reset";
     $message = "To reset your password, click on the following link: https://everydaychef.netlify.app/reset_password.php?email=" . urlencode($email);
     $headers = "From: obaejeoguabasilim@gmail.com";
-    
+
     // Send the email
     if (mail($email, $subject, $message, $headers)) {
         echo "An email with instructions to reset your password has been sent to $email";
     } else {
-        echo "Failed to send email. Please try again later.";
+        echo "Failed to send email. Please try again later. Error: " . error_get_last()['message'];
     }
 }
 ?>
